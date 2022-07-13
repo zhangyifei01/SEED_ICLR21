f852233b19613a372b3e2cc64402c8ccfa62dfdb
Yifei Zhang <44116483+zhangyifei01@users.noreply.github.com>
2022-07-13 23:41:52 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -4,7 +4,7 @@ CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python main_seed.py \
   --lr 0.03 \
   --batch-size 256 --seed-k 65536\
   --mlp --seed-st 0.2 --seed-tt 0.01 --aug-plus --cos \
-  --tpretrained ../checkpoint_0099.pth.tar \
+  --tpretrained ../teacher_workdir/checkpoint_0199.pth.tar \
   --dist-url 'tcp://localhost:10001' --multiprocessing-distributed --world-size 1 --rank 0 \
   /data1/ILSVRC2012
 
